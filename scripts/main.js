977779268acfa209f60269822b95308ab05fc065
Davina Leong <davina.leong.sy@gmail.com>
2022-11-23 16:29:12 +0800

    Able to toggle share footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 console.log(`main.js loaded`)
+
+const invertedAttr = `data-inverted`
+const openAttr = `data-open`
+
+const btnShareEl = document.querySelector(`[data-element="btn-share"]`)
+const cardFooterShareEl = document.querySelector(
+  `[data-element="card-footer-share"]`
+)
+
+btnShareEl.addEventListener(`click`, (e) => {
+  e.target.toggleAttribute(invertedAttr)
+  cardFooterShareEl.toggleAttribute(openAttr)
+})
